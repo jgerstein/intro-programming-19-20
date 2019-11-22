6eaa91ff5095827ad2ea4a87e1e564d998690f2b
jgerstein <jgerstein@ucvts.tec.nj.us>
2019-11-22 09:35:47 -0500

    starting git!

--- day20/scratchpad.py
+++ day20/scratchpad.py
@@ -1,41 +1,52 @@
 import code
+from random import randint
 
 class Character:
-    """Character class. Base class for all types of characters"""
+    """Character class"""
 
     def __init__(self, name, x=0, y=0):
-        """Constructor for Character class"""
+        """Constructor for the Character class"""
         self.name = name
         self.x = x
         self.y = y
 
-    def check_loc(self, x, y):
-        if x == self.x and y == self.y:
+    def is_here(self, x, y):
+        """Return True if the Character is at specified location, False otherwise"""
+        if self.x == x and self.y == y:
             return True
         else:
             return False
 
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
     def __repr__(self):
-        return f"<{self.name} @ {self.x}, {self.y}>" 
+        return f"< {self.name} @ {self.x},{self.y} >"
+
+
+def show_map():
+    for y in range(7):
+        for x in range(10):
+            occupied = False
+            # Everything in here refers to a cell with coordinates (x,y)
+            for c in characters:
+                if c.is_here(x,y):
+                    occupied = True
+            if occupied:
+                print("@", end="")
+            else:
+                print(f".", end="")
+        print()
 
 characters = [
-    Character('Harsh', 5, 4),
-    Character('Rachel', 3, 7),
-    Character('Cory'),
-    Character('Morgan', 2, 1),
-    Character('Myles', 1, 6)
-    ]
+    Character('Tommy', 5, 1),
+    Character('Markus', 4, 2),
+    Character('Choncho', 2, 3),
+    Character('Kevin'),
+    Character('Ethan', 8, 2),
+    Character('Melody', 7, 3)
+]
 
-for y in range(10):
-    for x in range(10):
-        occupied = False
-        for c in characters:
-            if c.check_loc(x, y):
-                occupied = True
-        if occupied:
-            print(' @ ', end='|')
-        else:
-            print(f"{x},{y}", end="|")
-    print()
-
+# print(characters)
 code.interact(local=locals())
---
+++ day24/PITCHME.md
@@ -0,0 +1,45 @@
+---
+# Intro to Programming
+
+#### Unit 2 Section A - Introduce Version Control
+---
+# Version Control
+---
+# What is it?
+
+* Version control keeps track of your files and changes to them
+* Enables you to have different versions without saving multiple copies
+---
+# Why does it matter?
+
+* Collaborating on documents is easier
+* Very hard to lose work
+* Can have different versions existing simultaneously
+---
+# Git
+
+We're going to start with [a bit of reading](https://github.com/jgerstein/intro-programming-19-20/blob/master/day24/git_story.md) and I'll go over each section with you
+---
+# Reading 1
+
+Read through the end of the Snapshots section
+---
+# Reading 2
+
+Read through the end of Tags
+---
+# Reading 3
+
+Read through the end of Offline
+---
+# Reading 4
+
+Read through the end of Merges
+---
+# Reading 5
+
+Read through rewriting history
+---
+# Reading 6
+
+Read through staging area
